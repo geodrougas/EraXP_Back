edbfba8108bec8fd3f88cf1631f4300b4853cda3
George Drougas <georgios.drougas@gmail.com>
2024-04-23 19:00:54 +0300

    Initial commit

---
+++ EraXP_Back/Controllers/WeatherController.cs
@@ -0,0 +1,26 @@
+using EraXP_Back;
+using Microsoft.AspNetCore.Mvc;
+
+
+[Route("[Controller]")]
+public class WeatherController : ControllerBase
+{
+    string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    
+    [HttpGet]
+    public ActionResult<IEnumerable<WeatherForecast>> Get()
+    {
+        var forecast =  Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                Summaries[Random.Shared.Next(Summaries.Length)]
+            ))
+            .ToArray();
+        return forecast;
+    }
+}
---
+++ EraXP_Back/WeatherForecast.cs
@@ -0,0 +1,6 @@
+namespace EraXP_Back;
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
